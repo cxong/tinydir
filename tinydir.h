2cf33f5b9ef9b8e8025c7c0b5c85a38f74dbad8a
BarbDev <BarbDevelop@gmail.com>
2014-08-20 13:53:36 +0200

    -surrounded some MSVC-only sections that I forgot -made it compatible C90 (all variable declaration moved to begin of functions)

--- tinydir.h
+++ tinydir.h
@@ -116,7 +116,9 @@ int _tinydir_file_cmp(const void *a, const void *b);
 _TINYDIR_FUNC
 int tinydir_open(tinydir_dir *dir, const char *path)
 {
-	WCHAR    str[_TINYDIR_PATH_MAX] = {L""};
+#ifdef _MSC_VER
+	WCHAR    str[_TINYDIR_PATH_MAX] = { L"" }; /* Forgot to put it in MSVC section, shoulb be portable now */
+#endif
 	if (dir == NULL || path == NULL || strlen(path) == 0)
 	{
 		errno = EINVAL;
@@ -141,9 +143,9 @@ int tinydir_open(tinydir_dir *dir, const char *path)
 #ifdef _MSC_VER
 	strcat(dir->path, "\\*");
 	MultiByteToWideChar(0, 0, dir->path, strlen(dir->path), str, strlen(dir->path) + 1);
-	LPCWSTR cstr = str;		//Variable created here :'(
-	//dir->_h = FindFirstFile(dir->path, &dir->_f);
-	dir->_h = FindFirstFile(cstr, &dir->_f);
+	/* LPCWSTR cstr = str;		 Not C90 compatible */
+	/* dir->_h = FindFirstFile(dir->path, &dir->_f); */
+	dir->_h = FindFirstFile(str, &dir->_f);
 	dir->path[strlen(dir->path) - 2] = '\0';
 	if (dir->_h == INVALID_HANDLE_VALUE)
 #else
@@ -307,6 +309,9 @@ int tinydir_next(tinydir_dir *dir)
 _TINYDIR_FUNC
 int tinydir_readfile(const tinydir_dir *dir, tinydir_file *file)
 {
+#ifdef _MSC_VER
+	char    str[_TINYDIR_FILENAME_MAX] = { "" }; /* Used to go from the WCHAR cFileName to a normal char */
+#endif
 	if (dir == NULL || file == NULL)
 	{
 		errno = EINVAL;
@@ -322,7 +327,7 @@ int tinydir_readfile(const tinydir_dir *dir, tinydir_file *file)
 		return -1;
 	}
 	if (strlen(dir->path) +
-		//strlen(
+		/* strlen( */
 #ifdef _MSC_VER
 		wcslen(
 			dir->_f.cFileName
@@ -337,7 +342,7 @@ int tinydir_readfile(const tinydir_dir *dir, tinydir_file *file)
 		errno = ENAMETOOLONG;
 		return -1;
 	}
-	//if (strlen(
+	/* if (strlen( */
 #ifdef _MSC_VER
 	if (wcslen(
 			dir->_f.cFileName
@@ -353,16 +358,15 @@ int tinydir_readfile(const tinydir_dir *dir, tinydir_file *file)
 
 	strcpy(file->path, dir->path);
 	strcat(file->path, "/");
-	//strcpy(file->name,
+	/* strcpy(file->name, */
 #ifdef _MSC_VER
-	char    str[_TINYDIR_FILENAME_MAX] = {""};
 	WideCharToMultiByte(0, 0, dir->_f.cFileName, wcslen(dir->_f.cFileName), str, wcslen(dir->_f.cFileName) + 1, NULL, NULL);
-	const char* cstr = str;		// Variable created here :'(
+	/* const char* cstr = str;		Not C90 compatible */
 	strcpy(file->name,
-		//dir->_f.cFileName
-		cstr
+		/* dir->_f.cFileName */
+		str
 #else
-	strcpy(file->name, // Line added
+	strcpy(file->name, /* moved here for other use than MSVC */
 		dir->_e->d_name
 #endif
 	);
