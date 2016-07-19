d1575e1285224a575e5a2ba345bcfc85c3344cf8
Lautis Sun <lautis0503@gmail.com>
2016-07-19 10:03:45 +0800

    Replace & with && in line 36&40
    Should look good

--- tinydir.h
+++ tinydir.h
@@ -33,11 +33,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 extern "C" {
 #endif
 
-#if ((defined _UNICODE) & !(defined UNICODE))
+#if ((defined _UNICODE) && !(defined UNICODE))
 #define UNICODE
 #endif
 
-#if ((defined UNICODE) & !(defined _UNICODE))
+#if ((defined UNICODE) && !(defined _UNICODE))
 #define _UNICODE
 #endif
 
