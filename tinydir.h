d5a460c903b9e7c55ffbd0d31fb3a9dfffdb6aa1
James Chen <jianhua.chen@cocos2d-x.org>
2017-03-29 20:41:57 +0800

    Fixes FindFirstFile is missing on win10 (#50, fixes #49)

--- tinydir.h
+++ tinydir.h
@@ -294,7 +294,7 @@ int tinydir_open(tinydir_dir *dir, const _tinydir_char_t *path)
 #ifdef _MSC_VER
 	_tinydir_strcpy(path_buf, dir->path);
 	_tinydir_strcat(path_buf, TINYDIR_STRING("\\*"));
-	dir->_h = FindFirstFile(path_buf, &dir->_f);
+	dir->_h = FindFirstFileEx(path_buf, FindExInfoStandard, &dir->_f, FindExSearchNameMatch, NULL, 0);
 	if (dir->_h == INVALID_HANDLE_VALUE)
 	{
 		errno = ENOENT;
