daf125f265724a2713445b94e7a06e973184ba12
Lautis Sun <lautis0503@gmail.com>
2021-11-11 23:34:31 +0800

    Use lstat() for macOS and *BSD (bug fix)
    Refer to issue #63. Thanks to @wojdyr.

--- tinydir.h
+++ tinydir.h
@@ -543,7 +543,9 @@ int tinydir_readfile(const tinydir_dir *dir, tinydir_file *file)
 	if (_tstat(
 #elif (defined _BSD_SOURCE) || (defined _DEFAULT_SOURCE)	\
 	|| ((defined _XOPEN_SOURCE) && (_XOPEN_SOURCE >= 500))	\
-	|| ((defined _POSIX_C_SOURCE) && (_POSIX_C_SOURCE >= 200112L))
+	|| ((defined _POSIX_C_SOURCE) && (_POSIX_C_SOURCE >= 200112L)) \
+	|| ((defined __APPLE__) && (defined __MACH__)) \
+	|| (defined BSD)
 	if (lstat(
 #else
 	if (stat(
